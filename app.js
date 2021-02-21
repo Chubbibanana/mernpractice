fb8e7d59145a428f27b351d43322bcd0297bedb9
Chubbibanana <dongsoo.tchah@gmail.com>
2021-02-20 20:15:02 -0800

    Backend user auth finished

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
 const app = express();
 
 const db = require('./config/keys').mongoURI;
@@ -8,7 +11,11 @@ mongoose
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.get('/', (req, res) => res.send("Hello MERN"));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 
